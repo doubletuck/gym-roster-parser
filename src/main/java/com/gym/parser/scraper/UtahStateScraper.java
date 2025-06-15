96e5b020b8e3651442e0bc0316aa9a12a92abe66
whyceewhite <ycwhite@gmail.com>
2025-06-15 15:10:15 -0400

    Scraper initialization for Utah State

---
+++ src/main/java/com/gym/parser/scraper/UtahStateScraper.java
@@ -0,0 +1,81 @@
+package com.gym.parser.scraper;
+
+import com.doubletuck.gym.common.model.AcademicYear;
+import com.doubletuck.gym.common.model.College;
+import com.gym.parser.model.Athlete;
+import com.gym.parser.util.EventParser;
+import com.gym.parser.util.LocationParser;
+import com.gym.parser.util.NameParser;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UtahStateScraper extends AbstractScraper {
+
+    public final static Logger logger = LoggerFactory.getLogger(UtahStateScraper.class);
+
+    public UtahStateScraper(Integer year) {
+        super(year);
+    }
+
+    public College getCollege() {
+        return College.UTAHSTATE;
+    }
+
+    String buildRosterUrl() {
+        return String.format("%s/%d?view=2",
+                "https://utahstateaggies.com/sports/womens-gymnastics/roster",
+                this.year);
+    }
+
+    Logger getLogger() {
+        return logger;
+    }
+
+    Elements selectAthleteTableRowsFromPage(Document document) {
+        Elements tables = document.select("table");
+        if (!tables.isEmpty()) {
+            for (Element table : tables) {
+                Element caption = table.selectFirst("caption");
+                if (caption != null && caption.text().toLowerCase().contains("gymnastics roster")) {
+                    return table.select("tbody tr");
+                }
+            }
+        }
+        return null;
+    }
+
+    Athlete parseAthleteRow(Element tableRowElement) {
+        Athlete athlete = null;
+
+        int nameIndex = 0;
+        int eventIndex = 2;
+        int academicYearIndex = 3;
+        int hometownIndex = 4;
+
+        Elements cells = tableRowElement.select("td");
+        if (cells.size() > 1) {
+            athlete = new Athlete();
+            athlete.setCollege(getCollege());
+            athlete.setYear(this.year);
+
+            String[] names = NameParser.parse(cells.get(nameIndex).text());
+            athlete.setFirstName(names[0]);
+            athlete.setLastName(names[1]);
+
+            athlete.setEvent(EventParser.parse(cells.get(eventIndex).text()));
+            athlete.setAcademicYear(AcademicYear.find(cells.get(academicYearIndex).text()));
+
+            String[] hometownClubCell = cells.get(hometownIndex).text().split("/");
+            LocationParser locationParser = new LocationParser(hometownClubCell.length > 0 ? hometownClubCell[0] : null);
+            locationParser.parse();
+            athlete.setHomeTown(locationParser.getTown());
+            athlete.setHomeState(locationParser.getState());
+            athlete.setHomeCountry(locationParser.getCountry());
+        }
+
+        return athlete;
+    }
+}
