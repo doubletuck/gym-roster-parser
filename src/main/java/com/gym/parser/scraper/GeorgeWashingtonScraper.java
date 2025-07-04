62c9f3d40441123b66ab118a32586553af9b3050
whyceewhite <ycwhite@gmail.com>
2025-07-03 20:53:50 -0400

    George Washington scraper, team roster export, coaching history

--- src/main/java/com/gym/parser/scraper/GeorgeWashingtonScraper.java
+++ src/main/java/com/gym/parser/scraper/GeorgeWashingtonScraper.java
@@ -1,5 +1,6 @@
 package com.gym.parser.scraper;
 
+import ch.qos.logback.core.hook.ShutdownHook;
 import com.doubletuck.gym.common.model.AcademicYear;
 import com.doubletuck.gym.common.model.College;
 import com.gym.parser.model.Athlete;
@@ -12,23 +13,22 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NewHampshireScraper extends AbstractScraper {
+public class GeorgeWashingtonScraper extends AbstractScraper {
 
-    private final static Logger logger = LoggerFactory.getLogger(NewHampshireScraper.class);
+    private final static Logger logger = LoggerFactory.getLogger(GeorgeWashingtonScraper.class);
 
-    public NewHampshireScraper(Integer year) {
+    public GeorgeWashingtonScraper(Integer year) {
         super(year);
     }
 
     public College getCollege() {
-        return College.NEWHAMPSHIRE;
+        return College.GEORGEWASHINGTON;
     }
 
     String buildRosterUrl() {
-        return String.format("%s/%d-%02d",
-                "https://unhwildcats.com/sports/womens-gymnastics/roster",
-                this.year-1,
-                this.year%100);
+        return String.format("%s/%d",
+                "https://gwsports.com/sports/womens-gymnastics/roster",
+                this.year);
     }
 
     Logger getLogger() {
@@ -52,29 +52,14 @@ public class NewHampshireScraper extends AbstractScraper {
         Athlete athlete = null;
         int nameIndex = 0;
         int academicYearIndex = 1;
-        int eventIndex = 2;
-        int clubIndex = 3;
-        int hometownIndex = 4;
+        int hometownIndex = 3;
+        int eventIndex = -1;
 
-        if (this.year <= 2024 && this.year > 2019) {
-            nameIndex = 1;
-            academicYearIndex = 3;
-            eventIndex = 5;
-            clubIndex = -1;
-            hometownIndex = 6;
-        } else if (this.year <= 2019 && this.year > 2015) {
+        if (this.year <= 2018) {
             nameIndex = 1;
             academicYearIndex = 2;
             eventIndex = 3;
-            hometownIndex = 5;
-            clubIndex = -1;
-        } else if (this.year == 2015) {
-            clubIndex = -1;
-        } else if (this.year <= 2014) {
-            nameIndex = 1;
-            academicYearIndex = 2;
-            eventIndex = 3;
-            clubIndex = -1;
+            hometownIndex = 4;
         }
 
         Elements cells = tableRowElement.select("td");
@@ -87,11 +72,10 @@ public class NewHampshireScraper extends AbstractScraper {
             athlete.setFirstName(names[0]);
             athlete.setLastName(names[1]);
 
-            athlete.setEvent(EventParser.parse(cells.get(eventIndex).text()));
-            athlete.setAcademicYear(AcademicYear.find(cells.get(academicYearIndex).text()));
-            if (clubIndex != -1) {
-                athlete.setClub(cells.get(clubIndex).text());
+            if (eventIndex != -1) {
+                athlete.setEvent(EventParser.parse(cells.get(eventIndex).text()));
             }
+            athlete.setAcademicYear(AcademicYear.find(cells.get(academicYearIndex).text()));
 
             String[] hometownCells = cells.get(hometownIndex).text().split("/");
             LocationParser locationParser = new LocationParser(hometownCells.length > 0 ? hometownCells[0] : null);
