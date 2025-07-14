18da6c60d4f729f56f7497fea4214680a77e150f
whyceewhite <ycwhite@gmail.com>
2025-07-13 21:10:01 -0400

    Brown scraper, team roster exports, coaching history

---
+++ src/main/java/com/gym/parser/scraper/BrownScraper.java
@@ -0,0 +1,82 @@
+package com.gym.parser.scraper;
+
+import com.doubletuck.gym.common.model.AcademicYear;
+import com.doubletuck.gym.common.model.College;
+import com.gym.parser.model.Athlete;
+import com.gym.parser.util.EventParser;
+import com.gym.parser.util.LocationParser;
+import com.gym.parser.util.NameParser;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BrownScraper extends AbstractScraper {
+
+    public final static Logger logger = LoggerFactory.getLogger(BrownScraper.class);
+
+    public BrownScraper(Integer year) {
+        super(year);
+    }
+
+    public College getCollege() {
+        return College.BROWN;
+    }
+
+    String buildRosterUrl() {
+        return String.format("%s/%d",
+                "https://brownbears.com/sports/womens-gymnastics/roster",
+                this.year);
+    }
+
+    Logger getLogger() {
+        return logger;
+    }
+
+    Document getPageDocument() {
+        return getPageDocumentWithButtonClick();
+    }
+
+    Elements selectAthleteTableRowsFromPage(Document document) {
+        Element table = document.selectFirst("div#rosterListPrint table");
+        if (table == null) {
+            return null;
+        }
+
+        return table.select("tbody tr");
+    }
+
+    Athlete parseAthleteRow(Element tableRowElement) {
+        Athlete athlete = null;
+
+        int nameIndex = 0;
+        int eventIndex = 1;
+        int academicYearIndex = 3;
+        int hometownIndex = 4;
+        int clubIndex = 6;
+
+        Elements cells = tableRowElement.select("td");
+        if (cells.size() > 1) {
+            athlete = new Athlete();
+            athlete.setCollege(getCollege());
+            athlete.setYear(this.year);
+
+            String[] names = NameParser.parse(cells.get(nameIndex).text());
+            athlete.setFirstName(names[0]);
+            athlete.setLastName(names[1]);
+
+            athlete.setEvent(EventParser.parse(cells.get(eventIndex).text()));
+            athlete.setAcademicYear(AcademicYear.find(cells.get(academicYearIndex).text()));
+            athlete.setClub(cells.get(clubIndex).text().trim());
+
+            String[] hometownCells = cells.get(hometownIndex).text().split("/");
+            LocationParser locationParser = new LocationParser(hometownCells.length > 0 ? hometownCells[0] : null);
+            locationParser.parse();
+            athlete.setHomeTown(locationParser.getTown());
+            athlete.setHomeState(locationParser.getState());
+            athlete.setHomeCountry(locationParser.getCountry());
+        }
+        return athlete;
+    }
+}
